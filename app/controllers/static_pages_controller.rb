acc9dab990f3f5639c778cd3c8bc250695b8ec03
Tristan Gruener <tristan.gruener@gmail.com>
2016-10-20 10:54:58 -0700

    set up static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,22 @@
 class StaticPagesController < ApplicationController
   def index
-
   end
 
-  
+  def services_and_specialties
+  end
+
+  def fees_forms_and_insurance
+  end
+
+  def mindful_education_project
+  end
+
+  def mindful_relating
+  end
+
+  def questions_for_reflection
+  end
+
+  def location_and_contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "static_pages#index"
+  root 'static_pages#index'
+
+  get '/services-and-specialties', to: 'static_pages#services_and_specialties'
+  get '/fees-forms-and-insurance', to: 'static_pages#fees_forms_and_insurance'
+  get '/mindful-education-project', to: 'static_pages#mindful_education_project'
+  get '/mindful-relating', to: 'static_pages#mindful_relating'
+  get '/questions-for-reflection', to: 'static_pages#questions_for_reflection'
+  get '/location-and-contact', to: 'static_pages#location_and_contact'
 
 end
