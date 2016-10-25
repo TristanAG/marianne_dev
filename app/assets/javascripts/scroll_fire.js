45c4959eb69404763b75223a2a570757a8da0de9
Tristan Gruener <tristan.gruener@gmail.com>
2016-10-25 09:45:58 -0700

    fixed sidebar with turbolinks:load check

--- app/assets/javascripts/scroll_fire.js
+++ app/assets/javascripts/scroll_fire.js
@@ -1,29 +1,56 @@
-$( document ).ready(function() {
-  console.log('ready to go');
-    $('#nav-fixed').hide();
-});
+// $( document ).on('turbolinks:load', function() {
+//   console.log('ready to go');
+//     $('#nav-fixed').hide();
+// });
+//
+// $(window).scroll(function (event) {
+//     var pos = 326;
+//     var scroll = $(window).scrollTop();
+//     var below = false;
+//
+//
+//     if(pos <= scroll){
+//       $('#nav-unfixed').hide();
+//       below = true;
+//     }else{
+//       below = false;
+//     }
+//
+//     if(below == true){
+//       $('#nav-fixed').show();
+//
+//     }
+//
+//     if(below == false){
+//       $('#nav-unfixed').show();
+//       $('#nav-fixed').hide();
+//     }
+//
+// });
 
-$(window).scroll(function (event) {
-    var pos = 326;
-    var scroll = $(window).scrollTop();
-    var below = false;
-
-
-    if(pos <= scroll){
+$( document ).on('turbolinks:load', function() {
+  $('#nav-fixed').hide();
+  var setState = function() {
+    if(length > yPos && width > xPos){
+      //use the fixed sidebar state
       $('#nav-unfixed').hide();
-      below = true;
-    }else{
-      below = false;
-    }
-
-    if(below == true){
       $('#nav-fixed').show();
-      
-    }
-
-    if(below == false){
+    }else{
+      //use the fluid sidebar state
       $('#nav-unfixed').show();
       $('#nav-fixed').hide();
     }
-
+  }
+  var xPos = 683;
+  var yPos = 240;
+  var length = $(window).scrollTop();
+  var width = $(window).width();
+  $( window ).scroll(function (event) {
+    length = $(window).scrollTop();
+    setState();
+  });
+  $( window ).resize(function() {
+    width = $(window).width();
+    setState();
+  });
 });
