d0ff2342b515fcf6cab14d9a839bda0bb99eeba2
Tristan Gruener <tristan.gruener@gmail.com>
2016-10-25 20:52:54 -0700

    fixed sidebar bug

--- app/assets/javascripts/scroll_fire.js
+++ app/assets/javascripts/scroll_fire.js
@@ -1,35 +1,7 @@
-// $( document ).on('turbolinks:load', function() {
-//   console.log('ready to go');
-//     $('#nav-fixed').hide();
-// });
-//
-// $(window).scroll(function (event) {
-//     var pos = 326;
-//     var scroll = $(window).scrollTop();
-//     var below = false;
-//
-//
-//     if(pos <= scroll){
-//       $('#nav-unfixed').hide();
-//       below = true;
-//     }else{
-//       below = false;
-//     }
-//
-//     if(below == true){
-//       $('#nav-fixed').show();
-//
-//     }
-//
-//     if(below == false){
-//       $('#nav-unfixed').show();
-//       $('#nav-fixed').hide();
-//     }
-//
-// });
-
 $( document ).on('turbolinks:load', function() {
+
   $('#nav-fixed').hide();
+
   var setState = function() {
     if(length > yPos && width > xPos){
       //use the fixed sidebar state
@@ -41,16 +13,21 @@ $( document ).on('turbolinks:load', function() {
       $('#nav-fixed').hide();
     }
   }
+
   var xPos = 683;
-  var yPos = 240;
+  var yPos = 300;
+
   var length = $(window).scrollTop();
   var width = $(window).width();
+
   $( window ).scroll(function (event) {
     length = $(window).scrollTop();
     setState();
   });
+
   $( window ).resize(function() {
     width = $(window).width();
     setState();
   });
+
 });
