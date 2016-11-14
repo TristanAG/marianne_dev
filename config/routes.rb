20b9237679d2abdfce778c0e75193009fa0f0f2f
Tristan Gruener <tristan.gruener@gmail.com>
2016-11-14 13:51:05 -0800

    initial calendar view

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   get '/services-and-specialties', to: 'static_pages#services_and_specialties'
   get '/fees-forms-and-insurance', to: 'static_pages#fees_forms_and_insurance'
   get '/mindful-education-project', to: 'static_pages#mindful_education_project'
+
+  get '/mindfulness-courses', to: 'courses#index'
+  #need to first implement friendly_id
+  get '/mindfulness-courses/new', to: 'courses#new'
+  get '/mindfulness-courses/edit', to: 'courses#edit'
+
   get '/mindful-relating', to: 'static_pages#mindful_relating'
   get '/questions-for-reflection', to: 'static_pages#questions_for_reflection'
   get '/location-and-contact', to: 'static_pages#location_and_contact'
