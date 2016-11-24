38ff5b1f88194e3cc162835ea2e60c7cb7675263
Tristan Gruener <tristan.gruener@gmail.com>
2016-11-24 10:59:04 -0800

    deactivate script on mobile view

--- app/assets/javascripts/scroll_fire.js
+++ app/assets/javascripts/scroll_fire.js
@@ -1,33 +1,41 @@
 $( document ).on('turbolinks:load', function() {
 
-  $('#nav-fixed').hide();
+  $('#nav-fixed').hide()
 
   var setState = function() {
-    if(length > yPos && width > xPos){
-      //use the fixed sidebar state
-      $('#nav-unfixed').hide();
-      $('#nav-fixed').show();
-    }else{
-      //use the fluid sidebar state
-      $('#nav-unfixed').show();
-      $('#nav-fixed').hide();
+    if(width > 1000){
+      if(length > yPos && width > xPos){
+        //use the fixed sidebar state
+        $('#nav-unfixed').hide()
+        $('#nav-fixed').show()
+      } else {
+        //use the fluid sidebar state
+        $('#nav-unfixed').show()
+        $('#nav-fixed').hide()
+      }
+    } else {
+      $('#nav-unfixed').hide()
+      $('#nav-fixed').hide()
     }
+
   }
 
-  var xPos = 683;
-  var yPos = 300;
+  var xPos = 683
+  var yPos = 300
 
-  var length = $(window).scrollTop();
-  var width = $(window).width();
+  var length = $(window).scrollTop()
+  var width = $(window).width()
+
 
   $( window ).scroll(function (event) {
-    length = $(window).scrollTop();
-    setState();
-  });
+    length = $(window).scrollTop()
+    setState()
+  })
 
   $( window ).resize(function() {
-    width = $(window).width();
-    setState();
-  });
+    width = $(window).width()
+    setState()
+  })
+
 
 });
