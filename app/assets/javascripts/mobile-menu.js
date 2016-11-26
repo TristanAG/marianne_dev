c929b63363e9255de5743458380e452cf20ba2c4
Tristan Gruener <tristan.gruener@gmail.com>
2016-11-26 01:29:00 -0800

    catchup

--- app/assets/javascripts/mobile-menu.js
+++ app/assets/javascripts/mobile-menu.js
@@ -1,16 +1,17 @@
-$( document ).on('turbolinks:load', function() {
-  //init state
-      var visible = false
-            $( '#mobile-menu' ).hide();
-  //menu button triggers visibility state
-    $( '#menu-button' ).click(function () {
-      if( !visible){
-          visible = true
-            $( '#mobile-menu' ).slideDown( 'slow' );
-      } else {
-          visible = false
-            $( '#mobile-menu' ).slideUp( 'slow' );
-      }
-    })
+$( document ).on( 'turbolinks:load', function() {
+//init state
+    var visible = false
+          $( '#mobile-menu' ).hide();
 
+  $( '#menu-button' ).click(function () {
+    if( !visible ){
+//open state
+        visible = true
+          $( '#mobile-menu' ).slideDown( 'slow' );
+    } else {
+//closed state
+        visible = false
+          $( '#mobile-menu' ).slideUp( 'slow' );
+    }
+  })
 })
