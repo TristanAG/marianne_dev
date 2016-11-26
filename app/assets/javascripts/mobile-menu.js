27c86f67305bd92afe6f1d11544208f5cb8d3e91
Tristan Gruener <tristan.gruener@gmail.com>
2016-11-26 01:23:01 -0800

    js

--- app/assets/javascripts/mobile-menu.js
+++ app/assets/javascripts/mobile-menu.js
@@ -1,13 +1,16 @@
 $( document ).on('turbolinks:load', function() {
-  $( '#mobile-menu' ).hide();
-  visible = false
-  $( '#menu-button' ).click(function () {
-    if(!visible){
-      $( '#mobile-menu' ).slideDown( 'slow' );
-      visible = true
-    } else {
-      $( '#mobile-menu' ).slideUp( 'slow' );
-      visible = false
-    }
-  })
+  //init state
+      var visible = false
+            $( '#mobile-menu' ).hide();
+  //menu button triggers visibility state
+    $( '#menu-button' ).click(function () {
+      if( !visible){
+          visible = true
+            $( '#mobile-menu' ).slideDown( 'slow' );
+      } else {
+          visible = false
+            $( '#mobile-menu' ).slideUp( 'slow' );
+      }
+    })
+
 })
