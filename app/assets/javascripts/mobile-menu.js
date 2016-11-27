764690809c6614cf6938186568d5c5cd8ada904e
Tristan Gruener <tristan.gruener@gmail.com>
2016-11-27 14:06:15 -0800

    more site content

--- app/assets/javascripts/mobile-menu.js
+++ app/assets/javascripts/mobile-menu.js
@@ -1,17 +1,15 @@
 $( document ).on( 'turbolinks:load', function() {
-//init state
-    var visible = false
-          $( '#mobile-menu' ).hide();
+  //init state
+  var visible = false
+  $( '#mobile-menu' ).hide()
 
   $( '#menu-button' ).click(function () {
     if( !visible ){
-//open state
-        visible = true
-          $( '#mobile-menu' ).slideDown( 'slow' );
+      visible = true
+      $( '#mobile-menu' ).slideDown( 'slow' )
     } else {
-//closed state
-        visible = false
-          $( '#mobile-menu' ).slideUp( 'slow' );
+      visible = false
+      $( '#mobile-menu' ).slideUp( 'slow' )
     }
   })
 })
