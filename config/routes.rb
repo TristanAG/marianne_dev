4499361bc65ad5ffa39a5f30a4f453d8dfe4ceac
Tristan Gruener <tristan.gruener@gmail.com>
2016-11-30 14:02:40 -0800

    fixed bug in routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,13 @@ Rails.application.routes.draw do
     get '/login', to: 'devise/sessions#new'
   end
 
-  resources :courses, :path => 'mindfulnesscourses'
+  resources :courses, :path => 'mindfulness-courses'
   get '/admin', to: 'courses#admin_index'
-  get '/mindfulnesscourses', to: 'courses#index'
-  get '/calendarofevents', to: 'courses#calendar'
+  get '/mindfulness-courses', to: 'courses#index'
+  get '/calendar-of-events', to: 'courses#calendar'
   get '/mindful-education-project', to: 'courses#mep_index'
-  get '/mindfulnesscourses/new', to: 'courses#new'
-  get '/mindfulnesscourses/edit', to: 'courses#edit'
+  get '/mindfulness-courses/new', to: 'courses#new'
+  get '/mindfulness-courses/edit', to: 'courses#edit'
 
   get '/services-and-specialties', to: 'static_pages#services_and_specialties'
   get '/fees-forms-and-insurance', to: 'static_pages#fees_forms_and_insurance'
