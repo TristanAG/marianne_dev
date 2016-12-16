7ccf3d873af058745ad4fc12f4dbea458d4e0ec7
Tristan Gruener <tristan.gruener@gmail.com>
2016-12-16 11:00:32 -0800

    set up rerouting for old urls

--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,25 @@ Rails.application.routes.draw do
   end
 
   resources :courses, :path => 'mindfulness-courses'
-  get '/admin', to: 'courses#admin_index'
-  get '/mindfulness-courses', to: 'courses#index'
-  get '/calendar-of-events', to: 'courses#calendar'
+  get '/admin',                     to: 'courses#admin_index'
+  get '/mindfulness-courses',       to: 'courses#index'
+  get '/calendar-of-events',        to: 'courses#calendar'
   get '/mindful-education-project', to: 'courses#mep_index'
-  get '/mindfulness-courses/new', to: 'courses#new'
-  get '/mindfulness-courses/edit', to: 'courses#edit'
+  get '/mindfulness-courses/new',   to: 'courses#new'
+  get '/mindfulness-courses/edit',  to: 'courses#edit'
 
-  get '/services-and-specialties', to: 'static_pages#services_and_specialties'
-  get '/fees-forms-and-insurance', to: 'static_pages#fees_forms_and_insurance'
-  get '/questions-for-reflection', to: 'static_pages#questions_for_reflection'
-  get '/location-and-contact', to: 'static_pages#location_and_contact'
-  get '/faqs', to: 'static_pages#faqs'
+  get '/services-and-specialties',  to: 'static_pages#services_and_specialties'
+  get '/fees-forms-and-insurance',  to: 'static_pages#fees_forms_and_insurance'
+  get '/questions-for-reflection',  to: 'static_pages#questions_for_reflection'
+  get '/location-and-contact',      to: 'static_pages#location_and_contact'
+  get '/faqs',                      to: 'static_pages#faqs'
 
+  #old routes pre-migration
+  get '/servicesspecialties.html',      to: redirect('/services-and-specialties')
+  get '/feesformsinsurance.html',       to: redirect('/fees-forms-and-insurance')
+  get '/calendarofevents.html',         to: redirect('/calendar-of-events')
+  get '/mindfulnesscourses.html',       to: redirect('/mindfulness_courses')
+  get '/mindfuleducationproject.html',  to: redirect('/mindful-education-project')
+  get '/questionsforreflection.html',   to: redirect('/questions-for-reflection')
+  get '/officelocation.html',           to: redirect('/location-and-contact')
 end
