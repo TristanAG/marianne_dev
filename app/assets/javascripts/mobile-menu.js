dc45c0d4c7044a690332b21cada871faf0d81661
Tristan Gruener <tristan.gruener@gmail.com>
2016-12-16 10:23:11 -0800

    fixed mobile menu script

--- app/assets/javascripts/mobile-menu.js
+++ app/assets/javascripts/mobile-menu.js
@@ -1,4 +1,5 @@
-$( document ).on( 'turbolinks:load', function() {
+// $( document ).on( 'turbolinks:load', function() {
+$( document ).ready( function() {
   //init state
   var visible = false
   $( '#mobile-menu' ).hide()
